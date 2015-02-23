cf8934f07b9d5a7b022d4030f8549f05e6391e35
pierre <pierre.schmidt@gmail.com>
2015-02-23 16:28:42 +0000

    Add parameters handling to the save method

--- sense/version.py
+++ sense/version.py
@@ -1,4 +1,5 @@
-VERSION = "0.0.8" # Add User-Agent and optional token encoding with sense.app_secret key.
+VERSION = "0.0.9" # Add parameters handling to the save method
+#"0.0.8" # Add User-Agent and optional token encoding with sense.app_secret key.
 #"0.0.7": Fix a bug in Feed instance_url (case of adressing a feed by type on a retrieved feed)
 # 0.0.6: Allow addressing feed by node uid + feed type
 # 0.0.5: Enable Node update
